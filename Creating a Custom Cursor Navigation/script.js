4fc6db8cbf483ef074a0547e93309a048d73ba7f
Anurag Chaudhary Tharu <anuragchaudharytharu371@gmail.com>
2024-07-10 20:48:43 +0545

    5th project second commit

--- Creating a Custom Cursor Navigation/script.js
+++ Creating a Custom Cursor Navigation/script.js
@@ -1,6 +1,7 @@
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', (event) => {
+  cursor.style.visibility = 'visible';
   cursor.style.left = event.x + 'px';
   cursor.style.top = event.y + 'px';
 });
